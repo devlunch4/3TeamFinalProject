398042745ca0d003ac80655a4013307d2c35a25b
wnsgur5303 <wnsgur5303@gmail.com>
2021-03-02 14:21:41 +0900

    commit

--- src/main/java/kr/or/ddit/fsurpport/web/FsurpportController.java
+++ src/main/java/kr/or/ddit/fsurpport/web/FsurpportController.java
@@ -1,5 +1,10 @@
 package kr.or.ddit.fsurpport.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("fsurpport")
+@Controller
 public class FsurpportController {
 
 }
