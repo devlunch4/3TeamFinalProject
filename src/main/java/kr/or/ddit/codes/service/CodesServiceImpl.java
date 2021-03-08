f15039484634d8bd6a997c82b5f305dcd89e535d
DESKTOP-082UI83\PC-20 <devlunch4@gmail.com>
2021-03-08 09:24:36 +0900

    20210308 0924 KKCversion merge fixed by KWS

--- src/main/java/kr/or/ddit/codes/service/CodesServiceImpl.java
+++ src/main/java/kr/or/ddit/codes/service/CodesServiceImpl.java
@@ -1,15 +1,11 @@
 package kr.or.ddit.codes.service;
 
-import java.util.List;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import kr.or.ddit.codes.repository.CodesDao;
 import kr.or.ddit.common.model.CodesVo;
-import kr.or.ddit.fanalysis.repository.FanalysisDao;
-import kr.or.ddit.farm.model.MsrrecVo;
 
 @Service("codesService")
 public class CodesServiceImpl implements CodesService {
@@ -22,7 +18,7 @@ public class CodesServiceImpl implements CodesService {
 
 	// 모든 코드정보 조회 03/06 (경찬)
 	@Override
-	public List<CodesVo> allCodes() {
+	public CodesVo allCodes() {
 		return codesDao.allCodes();
 	}
 
