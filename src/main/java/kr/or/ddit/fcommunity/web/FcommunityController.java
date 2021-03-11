bbbd61b2bc6fa05aef50c061d2e26abadcca5761
DESKTOP-082UI83\PC-20 <devlunch4@gmail.com>
2021-03-11 16:12:29 +0900

    fixed part user.xml ny kkc

--- src/main/java/kr/or/ddit/fcommunity/web/FcommunityController.java
+++ src/main/java/kr/or/ddit/fcommunity/web/FcommunityController.java
@@ -7,8 +7,10 @@ import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.or.ddit.farm.model.FcommunityVo;
 import kr.or.ddit.farm.model.FnoticeVo;
@@ -57,56 +59,6 @@ public class FcommunityController {
 		return "tiles.fcommunity.noticesModify";
 	}
 
-	// ggy_20210304 : 커뮤니티 미니장터 진입
-	@RequestMapping("minimarketView")
-	public String minimarketView(Model model) {
-
-		logger.debug("정보", commuityService.selectmarket());
-		model.addAttribute("noticelist", commuityService.selectmarket());
-
-		logger.debug("IN minimarketView()");
-
-		return "tiles.fcommunity.minimarketMain";
-	}
-
-	// ggy_20210304 : 커뮤니티 미니장터 상세정보 진입
-	@RequestMapping("minimarketInfoView")
-	public String minimarketInfoView(Model model, int market_no) {
-
-		model.addAttribute("detaillist", commuityService.selectonemarket(market_no));
-
-		logger.debug("IN minimarketInfoView()");
-		logger.debug("{}", market_no);
-
-		return "tiles.fcommunity.minimarketInfo";
-	}
-
-	// ggy_20210304 : 커뮤니티 미니장터 수정 페이지 진입
-	@RequestMapping("minimarketModifyView")
-	public String minimarketModify(Model model) {
-
-		logger.debug("IN minimarketModifyView()");
-
-		return "tiles.fcommunity.minimarketModify";
-	}
-
-	// ggy_20210304 : 커뮤니티 미니장터 글 작성 페이지 진입
-	@RequestMapping("minimarketRegistView")
-	public String minimarketRegistView(Model model) {
-
-		logger.debug("IN minimarketRegistView()");
-
-		return "tiles.fcommunity.minimarketRegist";
-	}
-
-	// shs_20210309 : 커뮤니티 미니장터 글 작성
-	@RequestMapping(path = "minimarketRegist", method = RequestMethod.POST)
-	public String minimarketRegist(Model model, FcommunityVo coVo) {
-
-		logger.debug("IN minimarketRegistView()");
-
-		return "tiles.fcommunity.minimarketRegist";
-	}
 
 	// --------------------------문의사항
 
