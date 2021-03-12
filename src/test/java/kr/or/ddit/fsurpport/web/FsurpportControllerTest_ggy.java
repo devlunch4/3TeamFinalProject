c5383e1280cc5c5655a1773e2d539f4c64899807
kgy9466 <kgy9466@gmail.com>
2021-03-12 10:12:19 +0900

    20210311_ggy_fsurpport : 영농일지 Test코드 selectMySimpleCodeInfoTest 까지 작성

--- src/test/java/kr/or/ddit/fsurpport/web/FsurpportControllerTest_ggy.java
+++ src/test/java/kr/or/ddit/fsurpport/web/FsurpportControllerTest_ggy.java
@@ -31,17 +31,94 @@ public class FsurpportControllerTest_ggy  extends WebTestConfig {
 	@Resource(name = "fsurpportService")
 	private FsurpportService fsurpportService;
 	
-	// 20210311_ggy_fsurpport : 영농일지 진입 controller Test
+	// 20210311_ggy_fsurpport : 영농일지 진입 controller Test_OK
 	@Test
-	public void process() throws Exception {
+	public void mainTest() throws Exception {
 		
 		mockMvc.perform(post("/fsurpport/main").param("user_id", "brown") )
 				.andExpect(view().name("tiles.fsurpport.fsurpportMain"))
-				.andExpect(model().attributeExists("farmdiaryList"))
-				.andExpect(model().attributeExists("workstepsList"))
-				.andExpect(model().attributeExists("itemsList"))
+				.andExpect(model().attributeExists("farmdiaryList", "workstepsList", "itemsList"))
 				.andExpect(status().isOk())
 				.andDo(print());
+	}
+	
+	// 20210313_ggy_fsurpport : 농업지원-영농일지 내 일지 목록 검색 Test_OK
+	@Test
+	public void searchAllFsurpportListTest() throws Exception {
+		
+		mockMvc.perform(post("/fsurpport/searchAllFsurpportList")
+				.param("startDate", "2021/03/12")
+				.param("endDate", "2021/03/12")
+				.param("item_code", "112")
+				.param("writer", "brown")
+				)
+		.andExpect(view().name("tiles.fsurpport.fsurpportMain"))
+		.andExpect(model().attributeExists("farmdiaryList", 
+				"workstepsList", "itemsList"))
+		.andExpect(status().isOk())
+		.andDo(print());
+	}
+	
+	// 20210313_ggy_fsurpport : 농업지원-영농일지 내 일지 상세조회를 위한 진입페이지 Test_OK
+	@Test
+	public void infoViewTest() throws Exception {
+		
+		mockMvc.perform(post("/fsurpport/infoView")
+				.param("f_diary_no", "23")
+				)
+		.andExpect(view().name("tiles.fsurpport.fsurpportInfo"))
+		.andExpect(model().attributeExists("farmdiaryList"
+					))
+		.andExpect(status().isOk())
+		.andDo(print());
+	}
+	
+	// 20210313_ggy_fsurpport : 농업지원-영농일지 내 일지 상세조회를 위한 진입페이지 Test_OK
+	@Test
+	public void insertViewTest() throws Exception {
+		
+		mockMvc.perform(post("/fsurpport/insertView")
+				.param("owner", "brown")
+				)
+		.andExpect(view().name("tiles.fsurpport.fsurpportInsert"))
+		.andExpect(model().attributeExists("mySimpleCodeList"
+				, "workstepsList", "itemsList", "b_typeList"
+				))
+		.andExpect(status().isOk())
+		.andDo(print());
+	}
+	
+	
+	// 20210313_ggy_fsurpport : 농업지원-영농일지 내 일지 상세조회를 위한 진입페이지 Test_OK
+	@Test
+	public void selectMySimpleCodeInsertViewTest() throws Exception {
+		
+		mockMvc.perform(post("/fsurpport/selectMySimpleCodeInsertView")
+				.param("owner", "brown")
+				.param("my_simple_code", "26")
+				)
+		.andExpect(view().name("tiles.fsurpport.fsurpportInsert"))
+		.andExpect(model().attributeExists("mySimpleCodeList"
+				, "workstepsList", "itemsList", "b_typeList"
+				))
+		.andExpect(status().isOk())
+		.andDo(print());
+	}
+	
+	// ggy_20210309 : 농업지원-영농일지 간편등록 목록 선택시 값 자동으로 배치_OK
+	@Test
+	public void selectMySimpleCodeInfoTest() throws Exception {
+		
+		mockMvc.perform(post("/fsurpport/selectMySimpleCodeInfo")
+				.param("user_id", "brown")
+				.param("my_simple_code", "26")
+				)
+		.andExpect(view().name("tiles.fsurpport.fsurpportInsert"))
+		.andExpect(model().attributeExists("mySimpleCodeList"
+				, "workstepsList", "itemsList", "selectMySimpleCodeInfo"
+				))
+		.andExpect(status().isOk())
+		.andDo(print());
 	}
 	
 	
